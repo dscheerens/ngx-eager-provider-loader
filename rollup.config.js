0dadccc8020e45b570a4db8a5e2766f86cdb7e86
Daan Scheerens <d.scheerens@gmail.com>
2017-01-15 16:26:52 +0100

    Added bundle task

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+	entry: './index.js',
+	dest: './bundles/index.js',
+	format: 'umd',
+	moduleName: 'ng.eagerProviderLoader',
+	globals: {
+		'@angular/core': 'ng.core'
+	},
+	external: [ '@angular/core' ]
+};
