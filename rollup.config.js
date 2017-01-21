a7d03da531e02e457114e3ea137e654df2dccc60
Daan Scheerens <d.scheerens@gmail.com>
2017-01-21 23:47:18 +0100

    Switch indentation style from tab to spaces

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 export default {
-	entry: './index.js',
-	dest: './bundles/index.js',
-	format: 'umd',
-	moduleName: 'ng.eagerProviderLoader',
-	globals: {
-		'@angular/core': 'ng.core'
-	},
-	external: [ '@angular/core' ]
+    entry: './index.js',
+    dest: './bundles/index.js',
+    format: 'umd',
+    moduleName: 'ng.eagerProviderLoader',
+    globals: {
+        '@angular/core': 'ng.core'
+    },
+    external: [ '@angular/core' ]
 };
--- src/eager-provider-loader.module.ts
+++ src/eager-provider-loader.module.ts
@@ -5,12 +5,12 @@ import { EAGER_PROVIDER } from './eager-provider.token';
 @NgModule()
 export class EagerProviderLoaderModule {
 
-	constructor(injector: Injector) {
-		// Obtain the set of tokens for the providers which need to be loaded eagerly.
-		const eagerProviderTokens: any[] = injector.get(EAGER_PROVIDER, []);
+    constructor(injector: Injector) {
+        // Obtain the set of tokens for the providers which need to be loaded eagerly.
+        const eagerProviderTokens: any[] = injector.get(EAGER_PROVIDER, []);
 
-		// Use injector to get each of the providers, which will force them to be instantiated.
-		eagerProviderTokens.forEach(injector.get.bind(injector));
-	}
+        // Use injector to get each of the providers, which will force them to be instantiated.
+        eagerProviderTokens.forEach(injector.get.bind(injector));
+    }
 
 }
